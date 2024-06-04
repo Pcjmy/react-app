7727fdd7ab972189bcdc7f1903660aca778f2601
Pcjmy <1716045191@qq.com>
2024-06-04 23:56:47 +0800

    feat: useContext

--- src/App.js
+++ src/App.js
@@ -1,76 +1,54 @@
-import React from 'react';
+import { createContext, useContext, useState } from 'react';
 
-class ScrollingList extends React.Component {
-  isAppend = true;
-  count = 0;
-  intervalId = 0;
-  constructor(props) {
-    super(props);
-    this.listRef = React.createRef();
-    this.state = { list: [] };
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    // Are we adding new items to the list?
-    // Capture the scroll position so we can adjust scroll later.
-    if (prevState.list.length < this.state.list.length) {
-      const list = this.listRef.current;
-      return list.scrollHeight - list.scrollTop;
-    }
-    return null;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    // If we have a snapshot value, we've just added new items.
-    // Adjust scroll so these new items don't push the old ones out of view.
-    // (snapshot here is the value returned from getSnapshotBeforeUpdate)
-    if (snapshot !== null) {
-      const list = this.listRef.current;
-      list.scrollTop = list.scrollHeight - snapshot;
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
-  appendData = () => {
-    if (this.isAppend) {
-      this.intervalId = setInterval(() => {
-        this.setState({
-          list: [...this.state.list, this.count++]
-        })
-      }, 1000);
-    } else {
-      clearInterval(this.intervalId);
-    }
-    this.isAppend = !this.isAppend;
-  }
-
-  render() {
-    return (
-      <div>
-        <input type="button" onClick={() => this.appendData()} value="追加/暂停追加数据" />
-        <div ref={this.listRef} style={{
-          overflow: 'auto',
-          height: '400px',
-          background: '#efefef',
-        }}>{this.state.list.map(item => {
-          return <div key={item} style={{
-            height: '60px',
-            padding: '10px',
-            marginTop: '10px',
-            border: '1px solid blue',
-            borderRadius: '6px',
-          }}>{item}</div>
-        })}</div>
-      </div>
-    );
-  }
-}
+const ThemeContext = createContext(null);
 
 function App() {
-  return <ScrollingList />;
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <Form />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light')
+          }}
+        />
+        Use dark mode
+      </label>
+    </ThemeContext.Provider>
+  )
+}
+
+function Form({ children }) {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'button-' + theme;
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  );
 }
 
 export default App;
